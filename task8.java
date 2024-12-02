251d8b5105729cc19620bd2b35fb27dd749a418d
asishgoswami <158997308+asishgoswami@users.noreply.github.com>
2024-12-02 12:34:04 +0530

    Add files via upload

---
+++ task8.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.Scanner;
+public class task8 {
+
+    //    static int josephus(int n, int k) {
+//        if (n == 1)
+//            return 0;
+//        else
+//            return (josephus(n - 1, k) + k - 1) % n + 1;
+//    }
+//
+//
+//public static void reverseArray(int[] array) {
+//        int start = 0;
+//        int end = array.length - 1;
+//        while(start<end){
+//            int temp=array[start];
+//            array[start]=array[end];
+//            array[end]=temp;
+//            start++;
+//            end--;
+//        }
+//    }
+//    public static int findmin(int[] array) {
+//        int min = array[0];
+//        for (int i = 1; i < array.length; i++) {
+//            if (array[i] < min) {
+//                min = array[i];
+//            }
+//        }
+//        return min;
+//    }
+
+//    public static int findmax(int[] array) {
+//        int max = array[0];
+//        for (int i = 1; i < array.length; i++) {
+//            if (array[i] > max) {
+//                max = array[i];
+//            }
+//        }
+//        return max;
+//    }
+
+
+    public static void main(String[] args) {
+//        int n = 14;
+//        int k = 2;
+//        System.out.println(josephus(n, k));
+//                int[] array = {1, 2, 3, 4, 5, 6, 7};
+//
+//                System.out.println("Original Array: ");
+//      for(int i:array){
+//          System.out.println(i);
+//      }
+//
+//                reverseArray(array);
+//
+//                System.out.println("\nReversed Array: ");
+//                for (int i : array) {
+//                    System.out.print(i + " ");
+            int[] array = {10, 20, 5, 15, 30, 25};
+
+//            int minValue = findmin(array);
+//            int maxValue = findmax(array);
+//
+//            System.out.println("Minimum  in the array: " + minValue);
+//            System.out.println("Maximum  in the array: " + maxValue);
+        }
+
+    }
+
+
+
+
+
+
